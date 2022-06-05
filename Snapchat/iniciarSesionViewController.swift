9b01f9324e615b16ada4212b65671dc638abff8e
mvillanueva24 <mariorvillanueva24@gmail.com>
2022-06-04 19:43:55 -0700

    Proyecto Snapchat con Google Sigin

--- Snapchat/iniciarSesionViewController.swift
+++ Snapchat/iniciarSesionViewController.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-import Firebase
 import FirebaseAuth
+import GoogleSignIn
 
 class iniciarSesionViewController: UIViewController {
 
@@ -16,20 +16,49 @@ class iniciarSesionViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Googleconf()
     }
+    
+    private func Googleconf(){
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance()?.delegate = self
+    }
+    
     @IBAction func iniciarSesionTapped(_ sender: Any) {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
             (user, error) in
             print("Intentando Iniciar Sesion")
-            if error != nil {
-                print("Se presento el siguiente error: (error)")
+            if let error = error{
+                print("Se presento el siguiente error: \(error)")
             }
             else{
                 print("Inicio de sesion exitoso")
             }
         }
     }
+    
+    @IBAction func GoogleSignin(_ sender: Any) {
+        GIDSignIn.sharedInstance()?.signIn()
+    }
 
 }
 
+extension iniciarSesionViewController: GIDSignInDelegate{
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error{
+            print("Error because \(error.localizedDescription)")
+            return
+        }
+
+        guard let auth = user.authentication else {return}
+        let credentials = GoogleAuthProvider.credential(withIDToken: auth.idToken, accessToken: auth.accessToken)
+
+        Auth.auth().signIn(with: credentials){ (authResult, error) in
+            if let error = error{
+                print("Error because \(error.localizedDescription)")
+                return
+            }
+        }
+    }
+
+}
